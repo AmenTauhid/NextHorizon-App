f0f908eaf74040dfde21c278faf4145a77343817
Ayman Tauhid <aymantauhid2@gmail.com>
2025-01-18 13:41:33 -0500

    Initial Commit

---
+++ NextHorizon/NextHorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextHorizonApp.swift
+//  NextHorizon
+//
+//  Created by Ayman Tauhid on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct NextHorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
